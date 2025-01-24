f8ee64fe129e9470837a02345d77549960526f3d
Devon Lambert <devon@realityknights.com>
2025-01-23 18:50:53 -0600

    Adds link data for social, page and footer

--- src/data.js
+++ src/data.js
@@ -4,3 +4,16 @@ export const pageLinks = [
     { id:3, href:'#services', text: 'services' },
     { id:4, href:'#tours', text: 'tours' }
 ]
+
+export const socialLinks = [
+    { id: 1, href: "https://www.facebook.com", icon: "facebook" },
+    { id: 2, href: "https://www.twitter.com", icon: "twitter" },
+    { id: 3, href: "https://www.squarespace.com", icon: "squarespace" }
+]
+
+export const footerLinks = [
+    { id:1, href:'#home', text: 'home' },
+    { id:2, href:'#about', text: 'about' },
+    { id:3, href:'#services', text: 'services' },
+    { id:4, href:'#featured', text: 'featured' }
+]
